6b0955467eba4ab1fa206d6e962fec0a5088abf8
Kazuki Takeuchi <k.take57@gmail.com>
2018-05-22 23:01:20 +0900

    questions1

---
+++ question1/index.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  const text = window.prompt('数字を入力してください。')
+  if (isNaN(text)) {
+    console.log('入力値は数字ではありません。')
+    return
+  }
+  const number = Number(text)
+  var range = Array.from(Array(number).keys()).map(function (val) {
+    return val + 1
+  })
+  range.forEach(function (val) {
+    let outputStr = ''
+    if (val % 3 === 0) {
+      outputStr = 'Fizz'
+    }
+    if (val % 5 === 0) {
+      outputStr += 'Buzz'
+    }
+    if (outputStr === '') {
+      outputStr = val
+    }
+    console.log(outputStr)
+  })
+}
